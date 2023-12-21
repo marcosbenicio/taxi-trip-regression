c05abfbd36f84102d0446d96e6939b0f28f22fc1
marcosbenicio <marcosbenicio0102@gmail.com>
2023-12-21 07:26:25 -0300

    Track large files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DataScience project of taxi trip duration using regression models',
+    author='Marcos Benício',
+    license='MIT',
+)
